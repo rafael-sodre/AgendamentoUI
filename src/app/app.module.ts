4f14bbb8513d74f7a9b63e784fbae9d027f24744
Rafael Sodré <rafael.sodre@me.com>
2019-11-20 14:26:21 -0300

    Ajustes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from "@angular/forms";
+import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
+
 import { PacientesComponent } from './pacientes/pacientes.component';
 import { PacienteComponent } from './Pacientes/paciente/paciente.component';
 import { PacienteListComponent } from './Pacientes/paciente-list/paciente-list.component';
 import { PacienteService } from './shared/paciente.service';
-import { FormsModule } from "@angular/forms";
-import { HttpClientModule } from '@angular/common/http';
+
 
 
 @NgModule({
